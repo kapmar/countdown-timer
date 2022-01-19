fb16c51f4034791c10b229a1c36f8442ccce9842
kapmar <98036617+kapmar@users.noreply.github.com>
2022-01-19 17:30:49 +0100

    removed comments/changed variables language

--- countdown_askisi.py
+++ countdown_askisi.py
@@ -1,19 +1,14 @@
-#ftiakse ena function me onoma countdown to opoio tha dexetai 1 argument seconds to opoio tha dinei o xristis
-#sti sunexeia otan kaleitai auto to function tha ektupwnei tous arithmous apo to ton arithmo pou edwse o xristis os to 1 me 1 deuterolepto diafora
-#telos tha ektupwnei ti leksi "finished" otan teliosei.
-#SIMANTIKO-------- EAN O XRISTIS DOSEI OTIDIPOTE ALLO EKTOS APO THETIKO ARITHMO TO PROGRAMMA NA TOU LEEI OTI EKANE LATHOS KAI PREPEI NA TO TREKSEI KSANA
-
 import time
 def countdown(seconds):
     while seconds > 0:
         print(seconds)
         seconds-= 1
         time.sleep(1)
-    print("teleiwsame")
+    print("finished")
 
-xronos = input("posa theleis na metrisume anapoda: ")
-if xronos.isdigit():
-    xronos_int = int(xronos)
-    countdown(xronos_int)
+count = input("how many seconds would you like to count down?: ")
+if count.isdigit():
+    count_int = int(count)
+    countdown(count_int)
 else:
-    print("vale numero")
+    print("how many seconds would you like to count down?")
